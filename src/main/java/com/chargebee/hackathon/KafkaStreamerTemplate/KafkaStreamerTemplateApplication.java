51c7321198a1d52565c1101dc96f60e5cb693687
cb-Ritchie <ritchie@chargebee.com>
2019-12-13 09:23:03 +0530

    Initial Spring template

---
+++ src/main/java/com/chargebee/hackathon/KafkaStreamerTemplate/KafkaStreamerTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.chargebee.hackathon.KafkaStreamerTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamerTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamerTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chargebee/hackathon/KafkaStreamerTemplate/KafkaStreamerTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chargebee.hackathon.KafkaStreamerTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamerTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
